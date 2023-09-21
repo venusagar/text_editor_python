5ef8f666918811d4d2541658ce6e2356a1146ebe
venusagar <141895122+venusagar@users.noreply.github.com>
2023-09-21 12:38:50 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+from tkinter import filedialog
+
+def open_file():
+    file_path = filedialog.askopenfilename()
+    if file_path:
+        with open(file_path, 'r') as file:
+            text.delete('1.0', tk.END)
+            text.insert(tk.END, file.read())
+
+def save_file():
+    file_path = filedialog.asksaveasfilename(defaultextension=".txt")
+    if file_path:
+        with open(file_path, 'w') as file:
+            file.write(text.get('1.0', tk.END))
+
+def clear_text():
+    text.delete('1.0', tk.END)
+
+root = tk.Tk()
+root.title("Simple Text Editor")
+
+menu = tk.Menu(root)
+root.config(menu=menu)
+
+file_menu = tk.Menu(menu)
+menu.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="Open", command=open_file)
+file_menu.add_command(label="Save", command=save_file)
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=root.quit)
+
+edit_menu = tk.Menu(menu)
+menu.add_cascade(label="Edit", menu=edit_menu)
+edit_menu.add_command(label="Clear", command=clear_text)
+
+text = tk.Text(root, wrap=tk.WORD)
+text.pack(expand=tk.YES, fill=tk.BOTH)
+
+root.mainloop()
